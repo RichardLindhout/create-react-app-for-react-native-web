cc7f9bf1d32f29120e86e08e9430c6b981cc202f
Richard Lindhout <richardlindhout96@gmail.com>
2018-12-12 22:19:06 +0100

    Update paths.js

--- config/paths.js
+++ config/paths.js
@@ -80,7 +80,7 @@ module.exports = {
   appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
-  appIndexJs: resolveModule(resolveApp, 'src/index.web'),
+  appIndexJs: resolveModule(resolveApp, 'index.web'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
   appTsConfig: resolveApp('tsconfig.json'),
